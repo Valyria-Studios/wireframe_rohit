f23c250f4bf0a8b74dc37b73729c5625a8ccfa70
rohityar <rohityarlagadda42@gmail.com>
2024-09-04 11:40:52 -0400

    tailwind edits

--- src/pages/WasteDistribution.js
+++ src/pages/WasteDistribution.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import StatsHeader from '../components/StatsHeader';
-import WasteChart from '../components/WasteChart';
-import Tabs from '../components/Tabs';
+import { Doughnut } from 'react-chartjs-2';
+import './WasteDistribution.css';
+
+const data = {
+  labels: ['Plastic', 'Paper & Cardboard', 'Kitchen Waste', 'Landfill', 'Metal', 'Glass'],
+  datasets: [
+    {
+      data: [27, 31, 22, 13, 5, 2],
+      backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#FF9F40', '#66BB6A', '#AB47BC'],
+      hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#FF9F40', '#66BB6A', '#AB47BC']
+    }
+  ]
+};
 
 function WasteDistribution() {
   return (
-    <div>
-      <StatsHeader />
-      <WasteChart />
-      <Tabs />
+    <div className="waste-distribution">
+      <h2>My Waste Distribution</h2>
+      <Doughnut data={data} />
+      <div className="tabs">
+        <button>Today</button>
+        <button>Week</button>
+        <button>Month</button>
+        <button className="active">All</button>
+      </div>
     </div>
   );
 }
